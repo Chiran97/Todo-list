1999d59d26cf652fc0110204e9120b79b27bfcfd
Chiran97 <chirandilanka@gmail.com>
2022-05-18 22:49:41 +0530

    initial commit

---
+++ src/modals/createTask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+
+const createTask = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default createTask;
