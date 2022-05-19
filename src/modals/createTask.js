a578f78cc7af874d7c8730038f5b5a0c76e666cc
Chiran97 <chirandilanka@gmail.com>
2022-05-19 23:31:48 +0530

    Show Task List

--- src/modals/createTask.js
+++ src/modals/createTask.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import React, { useState } from 'react';
+import { Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 
-const createTask = () => {
+const CreateTask = ({modal,toggle, save}) => {
+    const[taskName, setTaskName] = useState('');
+    const[description, setDescription] = useState('');
+
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value= e.target.value
+
+        if(name==="taskName"){
+            setTaskName(value)
+        }else
+            setDescription(value)
+    } 
+
+    const handleSave = () =>{
+        let taskObj={}
+        taskObj["Name"]= taskName
+        taskObj["Description"] = description
+        save(taskObj)
+    }
+
+
     return (
-        <div>
-            
-        </div>
+    <Modal isOpen={modal} toggle={toggle}>
+  
+       <ModalHeader toggle={toggle}> Create Task</ModalHeader>
+       <ModalBody>
+           <form>
+            <div className='form-group'>
+                <label>Task Name</label>
+               <input type="text" className="form-control" value={taskName} onChange={handleChange} name="taskName"/> 
+            </div>
+            <div className='form-group'>
+                <label >Description</label>
+                <textarea rows="5" cols="" className="form-control" value={description} onChange={handleChange} name="description"></textarea>
+            </div>     
+           </form>
+        </ModalBody>
+    <ModalFooter>
+        <Button color="primary" onClick={handleSave}>Create</Button>
+        {' '}
+        <Button onClick={toggle}>Cancel</Button>
+    </ModalFooter>
+  </Modal>
     );
 };
 
-export default createTask;
+export default CreateTask;
