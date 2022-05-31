e4d72c10936c68e322053527cebbdd63e11714a6
Chiran97 <chirandilanka@gmail.com>
2022-05-31 13:55:15 +0530

    todo cards

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import CreateTask from '../modals/createTask'
+import Card from './Card';
 
 const TodoList = () => {
    const [modal, setModal] = useState(false);
    const [taskList, setTaskList] = useState([]);
+
+
+   useEffect(() => {
+      let arr = localStorage.getItem("taskList")
+      
+      if(arr){
+         let obj = JSON.parse(arr)
+         setTaskList(obj)
+      }
+   },[])
 
 
 
@@ -14,6 +25,7 @@ const TodoList = () => {
    const saveTask = (taskObj) => {
       let tempList = taskList
       tempList.push(taskObj)
+      localStorage.setItem("taskList",JSON.stringify(tempList))
       setTaskList(tempList)
       setModal(false)
 
@@ -30,7 +42,7 @@ const TodoList = () => {
         </div>
 
         <div className='task-container'>
-           {taskList.map((obj) => <li>{obj.Name}</li>)} 
+           {taskList && taskList.map((obj,index) => <Card taskObj = {obj} index={index}/>)} 
         </div>
         <CreateTask toggle={toggle} modal={modal} save={saveTask}/>
        </> 
